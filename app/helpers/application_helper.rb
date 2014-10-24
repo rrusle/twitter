7ee46f7d8c1a91a51bb0d0dad7f4c7932324d83d
MarkBenny <1markbenny@gmail.com>
2014-10-24 14:55:55 +1100

    login created

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,23 +1,22 @@
 module ApplicationHelper
 
-	# def intellinav
+	def intellinav
     
- #    nav = ''
+    nav = ''
     
- #    if @current_user.present?
- #      nav += "<li>Hello #{ @current_user.name }</li> "
- #      nav += "<li>" + link_to('All users', users_path) + "</li>"
- #      nav += "<li>" + link_to('Post', posts_path) + "</li>"
- #      nav += "<li>" + link_to('New Post', new_post_path) + "</li> "
- #      nav += "<li>" + link_to('Edit profile', edit_user_path(@current_user)) + "</li> "
- #      nav += "<li>#{ link_to('Sign out', login_path, :method => :delete, :data => {:confirm => 'Are you sure?'}) }</li>"
- #    else
- #    	nav += "<li>" + link_to('Home', root_path) + "</li>"
- #      nav += "<li>#{ link_to('Sign up', new_user_path) }</li> "
- #      nav += "<li>#{ link_to('Sign in', login_path) }</li>"
- #    end
+    if @current_user.present?
+      nav += "<li>Hello #{ @current_user.name }</li> "
+      nav += "<li>" + link_to('All users', users_path) + "</li>"
+      nav += "<li>" + link_to('Tweet', tweets_path) + "</li>"
+      nav += "<li>" + link_to('New Tweet', new_tweet_path) + "</li> "
+      nav += "<li>" + link_to('Edit profile', edit_user_path(@current_user)) + "</li> "
+      nav += "<li>#{ link_to('Sign out', login_path, :method => :delete, :data => {:confirm => 'Are you sure?'}) }</li>"
+    else
+    	nav += "<li>" + link_to('Home', root_path) + "</li>"
+      nav += "<li>#{ link_to('Sign up', new_user_path) }</li> "
+      nav += "<li>#{ link_to('Sign in', login_path) }</li>"
+    end
     
- #    nav
-    
- #  end
+    nav
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
- root :to => 'users#index'
+ root :to => 'users#home'
 
  resources :users
  resources :tweets
