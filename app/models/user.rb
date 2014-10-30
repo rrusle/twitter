327a0cfa8d3f48cbc16ab7f34f84b9c09be8dbf1
rrusle <rush.lee@hotmail.com>
2014-10-30 16:04:44 +1100

    merged task for js

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@
 class User < ActiveRecord::Base
 	has_secure_password
 
-	has_many :tweets, dependent: :destroy 
+	has_many :tweets, dependent: :destroy
 
 	has_many :active_relationships, class_name:  "Relationship",
                                   foreign_key: "follower_id",
